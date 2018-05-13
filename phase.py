ed0cdecd5d77aa390ad96a901504586dedf9f6a6
zac <zac.kemp@outlook.com>
2018-05-13 10:57:16 +1000

    Fixed "scale" to center around middle of image and pad with ones

--- phase.py
+++ phase.py
@@ -143,10 +143,7 @@ class PhaseImagingSystem(object):
         """
 
         h, w = img.shape[:2]
-
         # width and height of the zoomed image
-        zh = int(np.round(zoom_factor * h))
-        zw = int(np.round(zoom_factor * w))
 
         # for multichannel images we don't want to apply the zoom factor to the RGB
         # dimension, so instead we create a tuple of zoom factors, one per array
@@ -155,18 +152,22 @@ class PhaseImagingSystem(object):
 
         # zooming out
         if zoom_factor < 1:
+            zh = int(np.round(zoom_factor * h))
+            zw = int(np.round(zoom_factor * w))
             # bounding box of the clip region within the output array
             top = (h - zh) // 2
             left = (w - zw) // 2
-            # zero-padding
-            out = np.zeros_like(img)
+            # pad with intensity of unity
+            out = np.ones_like(img)
             out[top:top + zh, left:left + zw] = zoom(img, zoom_tuple, **kwargs)
 
         # zooming in
         elif zoom_factor > 1:
             # bounding box of the clip region within the input array
-            top = (zh - h) // 2
-            left = (zw - w) // 2
+            zh = int(np.round(h / zoom_factor))
+            zw = int(np.round(w / zoom_factor))
+            top = (h - zh) // 2
+            left = (w - zw) // 2
             out = zoom(img[top:top + zh, left:left + zw], zoom_tuple, **kwargs)
             # `out` might still be slightly larger than `img` due to rounding, so
             # trim off any extra pixels at the edges
@@ -177,6 +178,7 @@ class PhaseImagingSystem(object):
         # if zoom_factor == 1, just return the input array
         else:
             out = img
+
         return out
     def rotate_images(self, std, mode, n_images):
         if n_images == 3:
