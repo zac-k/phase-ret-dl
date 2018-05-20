26356b21210be2c6b7029226788bee3c70033552
zac <zac.kemp@outlook.com>
2018-05-21 07:52:51 +1000

    Adds ability to plot errors for a single test set from details files

--- generate_csv.py
+++ generate_csv.py
@@ -7,37 +7,86 @@ different errors.txt file (and typically a different ANN configuration).
 
 import csv
 
-# List of input filenames
-filenames = ['errors0.txt','errors1.txt', 'errors2.txt', 'errors5.txt', 'errors10.txt', 'errors15.txt', 'errors20.txt', 'errors25.txt', 'errors30.txt', 'errors35.txt', 'errors40.txt', 'errors45.txt', 'errors50.txt', 'errors55.txt', 'errors60.txt']
 
-# Iterate over the files and extract the TIE and ANN errors into two
-# separate lists of lists.
-tie = []
-ann = []
-for filename in filenames:
-    col = []
-    with open(filename, 'r') as csvinput:
-        reader = csv.reader(csvinput, delimiter=' ')
+def single():
+    path = './data/figures/details/'
+    prefix = 'test_'
+    ext = '.txt'
+    n_samples = 1000
 
-        for row in reader:
-            if 'test' in row and 'input' in row:
-                error = row[6]
-                col.append(error[:-1])
-    set_size = int(len(col)/2)
-    tie.append(col[:set_size])
-    ann.append(col[set_size:])
+    sources = ['Rotation', 'Scale', 'Shift', 'Noise']
+    for j, source in enumerate(sources):
+        vars()[source] = []
 
-# Transpose both lists so that each column represents the data from a single input file
-tie = list(map(list, zip(*tie)))
-ann = list(map(list, zip(*ann)))
+    tie_error = []
+    ann_error = []
 
-# Write the output of each list to a separate csv file
-with open('output_TIE.csv','w') as output:
-    writer = csv.writer(output, lineterminator='\n')
-    writer.writerows(tie)
-with open('output_ANN.csv', 'w') as output:
-    writer = csv.writer(output, lineterminator='\n')
-    writer.writerows(ann)
+    for i in range(0, n_samples):
+        file = path + prefix + str(i) + ext
+        with open(file, 'r') as csvinput:
+            reader = csv.reader(csvinput, delimiter=' ')
+            for j in range(len(sources)):
+                next(reader)
+            tie_error.append(next(reader)[-1][:-1])
+            ann_error.append(next(reader)[-1][:-1])
 
 
+        for source in sources:
+            with open(file, 'r') as csvinput:
+                reader = csv.reader(csvinput, delimiter=' ')
+
+                for j, row in enumerate(reader):
+                    if source+':' in row and 'NA' not in row:
+                        x = row[2][:-1]
+
+                        vars()[source].append(x)
+    errors = []
+    for source in sources:
+        if vars()[source] != []:
+            row = [source] + vars()[source]
+            errors.append(row)
+    errors.append(['tie']+tie_error)
+    errors.append(['ann']+ann_error)
+
+    errors = list(map(list, zip(*errors)))
+    with open('errors_single.csv', 'w') as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerows(errors)
+
+
+def multiple():
+
+    # List of input filenames
+    filenames = ['errors0.txt','errors1.txt', 'errors2.txt', 'errors3.txt', 'errors4.txt', 'errors5.txt']
+
+    # Iterate over the files and extract the TIE and ANN errors into two
+    # separate lists of lists.
+    tie = []
+    ann = []
+    for filename in filenames:
+        col = []
+        with open(filename, 'r') as csvinput:
+            reader = csv.reader(csvinput, delimiter=' ')
+
+            for row in reader:
+                if 'test' in row and 'input' in row:
+                    error = row[6]
+                    col.append(error[:-1])
+        set_size = int(len(col)/2)
+        tie.append(col[:set_size])
+        ann.append(col[set_size:])
+
+    # Transpose both lists so that each column represents the data from a single input file
+    tie = list(map(list, zip(*tie)))
+    ann = list(map(list, zip(*ann)))
+
+    # Write the output of each list to a separate csv file
+    with open('output_TIE.csv','w') as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerows(tie)
+    with open('output_ANN.csv', 'w') as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerows(ann)
+
+single()
 
